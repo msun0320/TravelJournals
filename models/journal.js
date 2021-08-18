e8dbbf4b0e462481137d28095d7a164c40d13437
Mingyue Sun <mingyueees@gmail.com>
2021-08-18 11:44:42 -0700

    Add the review model

--- models/journal.js
+++ models/journal.js
@@ -8,6 +8,12 @@ const JournalSchema = new Schema({
   date: Date,
   text: String,
   public: Boolean,
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Journal", JournalSchema);
