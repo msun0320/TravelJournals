d3b79cb19514c022509e863437a80bd62d7469e2
Mingyue Sun <mingyueees@gmail.com>
2021-08-18 21:58:24 -0700

    Configure session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 
@@ -26,9 +27,22 @@ const app = express();
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "secretplaceholder",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 app.use("/journals", journals);
 app.use("/journals/:id/comments", comments);
