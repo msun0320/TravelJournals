e943409f04cc35d0c8c8b0dfb019423c11f6855b
Mingyue Sun <mingyueees@gmail.com>
2021-08-18 20:30:17 -0700

    Break out comment routes

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const { journalSchema, commentSchema } = require("./schemas.js");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
-const Journal = require("./models/journal");
-const Comment = require("./models/comment");
 
 const journals = require("./routes/journals");
+const comments = require("./routes/comments");
 
 mongoose.connect("mongodb://localhost:27017/travel-journal", {
   useNewUrlParser: true,
@@ -31,53 +28,12 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-const validateJournal = (req, res, next) => {
-  const { error } = journalSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-const validateComment = (req, res, next) => {
-  const { error } = commentSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
 app.use("/journals", journals);
+app.use("/journals/:id/comments", comments);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-app.post(
-  "/journals/:id/comments",
-  validateComment,
-  catchAsync(async (req, res) => {
-    const journal = await Journal.findById(req.params.id);
-    const comment = new Comment(req.body.comment);
-    journal.comments.push(comment);
-    await comment.save();
-    await journal.save();
-    res.redirect(`/journals/${journal._id}`);
-  })
-);
-
-app.delete(
-  "/journals/:id/comments/:commentId",
-  catchAsync(async (req, res) => {
-    const { id, commentId } = req.params;
-    await Journal.findByIdAndUpdate(id, { $pull: { comments: commentId } });
-    await Comment.findByIdAndDelete(commentId);
-    res.redirect(`/journals/${id}`);
-  })
-);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
---
+++ routes/comments.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const Journal = require("../models/journal");
+const Comment = require("../models/comment");
+const { commentSchema } = require("../schemas.js");
+const ExpressError = require("../utils/ExpressError");
+const catchAsync = require("../utils/catchAsync");
+
+const validateComment = (req, res, next) => {
+  const { error } = commentSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+router.post(
+  "/",
+  validateComment,
+  catchAsync(async (req, res) => {
+    const journal = await Journal.findById(req.params.id);
+    const comment = new Comment(req.body.comment);
+    journal.comments.push(comment);
+    await comment.save();
+    await journal.save();
+    res.redirect(`/journals/${journal._id}`);
+  })
+);
+
+router.delete(
+  "/:commentId",
+  catchAsync(async (req, res) => {
+    const { id, commentId } = req.params;
+    await Journal.findByIdAndUpdate(id, { $pull: { comments: commentId } });
+    await Comment.findByIdAndDelete(commentId);
+    res.redirect(`/journals/${id}`);
+  })
+);
+
+module.exports = router;
