e7bee2a437b3e3a530b38ed057c695cb5dd83991
Mingyue Sun <mingyueees@gmail.com>
2021-08-19 02:17:27 -0700

    Configure Passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const journals = require("./routes/journals");
 const comments = require("./routes/comments");
@@ -46,10 +49,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
+});
+
+app.get("/fakeUser", async (req, res) => {
+  const user = new User({ email: "example@gmail.com", username: "johndoe" });
+  const newUser = await User.register(user, "samplepassport");
+  res.send(newUser);
 });
 
 app.use("/journals", journals);
