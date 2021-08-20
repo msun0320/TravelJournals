7dd56fbd8c46d3c5f0ea5c517399c9bfab4853ee
Mingyue Sun <mingyueees@gmail.com>
2021-08-20 10:23:40 -0700

    Center map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 4, // starting zoom
+  center: journal.geometry.coordinates, // starting position [lng, lat]
+  zoom: 10, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat(journal.geometry.coordinates).addTo(map);
