32e754e4c9cf5943d96653b4022f4f02ba34cd3e
Mingyue Sun <mingyueees@gmail.com>
2021-08-20 03:29:23 -0700

    Add the multer middleware

--- routes/journals.js
+++ routes/journals.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const journals = require("../controllers/journals");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateJournal } = require("../middleware");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const Journal = require("../models/journal");
 
 router
   .route("/")
   .get(catchAsync(journals.index))
-  .post(isLoggedIn, validateJournal, catchAsync(journals.createJournal));
+  // .post(isLoggedIn, validateJournal, catchAsync(journals.createJournal));
+  .post(upload.array("image"), (req, res) => {
+    res.send(req.body, req.files);
+  });
 
 router.get("/new", isLoggedIn, journals.renderNewForm);
 
