ff16e2d56bedcfb6d51845d19ea1acfb23a8cc05
Mingyue Sun <mingyueees@gmail.com>
2021-08-20 10:48:22 -0700

    Customize map popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,9 +1,14 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  style: "mapbox://styles/mapbox/light-v10", // style URL
   center: journal.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(journal.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(journal.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${journal.location}</h3>`)
+  )
+  .addTo(map);
