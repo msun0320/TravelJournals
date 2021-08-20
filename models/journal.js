0ac715484bda2483ce622179afa19dd1a2ac0f5a
Mingyue Sun <mingyueees@gmail.com>
2021-08-20 07:43:47 -0700

    Add a thumbnail virtual property

--- models/journal.js
+++ models/journal.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 const Comment = require("./comment");
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
 const JournalSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   location: String,
   date: Date,
   text: String,
