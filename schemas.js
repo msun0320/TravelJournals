0cad681127984bc501ab07d05fb0b70c65e1bf01
Mingyue Sun <mingyueees@gmail.com>
2021-08-21 03:33:31 -0700

    Sanitize html w/ Joi

--- schemas.js
+++ schemas.js
@@ -1,12 +1,35 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHTML = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHTML(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.journalSchema = Joi.object({
   journal: Joi.object({
-    title: Joi.string().required(),
-    // image: Joi.string().required(),
-    location: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
     date: Joi.date().required(),
-    text: Joi.string().required(),
+    text: Joi.string().required().escapeHTML(),
     public: Joi.boolean(),
   }).required(),
   deleteImages: Joi.array(),
@@ -14,6 +37,6 @@ module.exports.journalSchema = Joi.object({
 
 module.exports.commentSchema = Joi.object({
   comment: Joi.object({
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
   }).required(),
 });
