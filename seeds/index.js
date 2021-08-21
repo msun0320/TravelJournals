a288c21e675d17c43bb077364019698cf94a1ac5
Mingyue Sun <mingyueees@gmail.com>
2021-08-20 21:43:42 -0700

    Add map controls

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const journal = new Journal({
-      author: "611f071e7031d083d82d9507",
+      author: "61200d46bb2c1a34f431d8f3",
       title: "Aenean tempor",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       date: "2018-12-09",
