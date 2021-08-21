a6136ecde7bd8ab45b540022df8b1b6e1e097173
Mingyue Sun <mingyueees@gmail.com>
2021-08-20 22:01:35 -0700

    Add mongo sanitize

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
+const mongoSanitize = require("express-mongo-sanitize");
+
 const userRoutes = require("./routes/users");
 const journalRoutes = require("./routes/journals");
 const commentRoutes = require("./routes/comments");
@@ -40,6 +42,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 
 const sessionConfig = {
   secret: "secretplaceholder",
